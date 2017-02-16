1b8dc93f1b28430e60290b3d9420be9b9229ed92
worldxaker <djxakir714@gmail.com>
2017-02-17 02:54:42 +0300

    test localstorage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'app works!!!';
+  text:string = "kek"
+  array:string[] = ["test"]
+  constructor (){
+    //this.array[this.array.length]= "test 1"
+    this.load()
+  }
+
+  but(){
+    //this.text="lol";
+    this.array[this.array.length]=this.text;
+  } 
+  save(){
+    localStorage.setItem("data", JSON.stringify(this.array))
+  }
+
+  load(){
+    if(localStorage.getItem("data")!=null)
+      this.array=JSON.parse(localStorage.getItem("data"))
+  }
+  delete(){
+    localStorage.setItem("data", JSON.stringify([]))
+  }
+  del(i){
+    this.array.splice(i,1)
+  }
 }
