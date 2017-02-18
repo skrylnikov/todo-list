4d6d2b52a0662647d120dbebd10551ea84b92608
worldxaker <djxakir714@gmail.com>
2017-02-18 18:46:30 +0300

    add save on localStorage

--- src/app/shared/todo.service.ts
+++ src/app/shared/todo.service.ts
@@ -1,7 +1,14 @@
-import { Todo } from './todo'
+import { Todo } from './todo';
 
 export class TodoService {
+
     todos: Todo[] = [new Todo('Create todo list'), new Todo('Play Dota 2', true)]
+
+    constructor() {
+        if (localStorage.getItem('todos') != null) {
+            this.todos = JSON.parse(localStorage.getItem('todos'));
+        }
+    }
 
     getTodos(): Todo[] {
         return this.todos;
@@ -11,6 +18,7 @@ export class TodoService {
         if (title.length > 0) {
             let todo: Todo = new Todo(title);
             this.todos.push(todo);
+            this.saveChange();
         }
     }
 
@@ -18,11 +26,17 @@ export class TodoService {
         let i = this.todos.indexOf(todo)
         if (i > -1) {
             this.todos.splice(i, 1);
+            this.saveChange();
         }
     }
 
     toggleTodo(todo: Todo) {
         todo.completed = !todo.completed;
+        this.saveChange();
+    }
+
+    saveChange(){
+        localStorage.setItem('todos', JSON.stringify(this.todos));
     }
 
 }
